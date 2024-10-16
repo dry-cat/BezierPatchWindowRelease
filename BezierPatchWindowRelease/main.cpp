32a028026eb098c1b8d6baa454df8228c9e1014b
dry-cat <ttwq5782@leeds.ac.uk>
2024-10-16 16:35:56 +0100

    Uncomment main code

--- BezierPatchWindowRelease/main.cpp
+++ BezierPatchWindowRelease/main.cpp
@@ -94,53 +94,53 @@ int main(int argc, char **argv)
     std::cout << "b2 = " << b2 << '\n';
     std::cout << "b3 = " << b3 << '\n';
 
-    // // initialize QT
-    // QApplication renderApp(argc, argv);
+    // initialize QT
+    QApplication renderApp(argc, argv);
 
-    // // check the args to make sure there's an input file
-    // if (argc != 2)
-    //     { // bad arg count
-    //     // print an error message
-    //     std::cout << "Usage: " << argv[0] << " textfile (.txt) containing the control points" << std::endl;
-    //     // and leave
-    //     return 0;
-    //     } // bad arg count
+    // check the args to make sure there's an input file
+    if (argc != 2)
+        { // bad arg count
+        // print an error message
+        std::cout << "Usage: " << argv[0] << " textfile (.txt) containing the control points" << std::endl;
+        // and leave
+        return 0;
+        } // bad arg count
 
 
-    // ControlPoints bezierPatch;
+    ControlPoints bezierPatch;
 
-    // // open the input files for the geometry & texture
-    // std::ifstream geometryFile(argv[1]);
+    // open the input files for the geometry & texture
+    std::ifstream geometryFile(argv[1]);
 
-    // // try reading it
-    // if (!(geometryFile.good()))
-    // {
-    //     std::cout << "Read failed for object " << argv[1] << std::endl;
-    //     return 0;
-    // } // object read failed
+    // try reading it
+    if (!(geometryFile.good()))
+    {
+        std::cout << "Read failed for object " << argv[1] << std::endl;
+        return 0;
+    } // object read failed
 
-    // bezierPatch = ControlPoints::ReadPointStream(geometryFile);
+    bezierPatch = ControlPoints::ReadPointStream(geometryFile);
 
-    // if(bezierPatch.vertices.size() == 0){
-    //     std::cout << "Read failed for control points " << argv[1] << std::endl;
-    //     return 0;
-    // } // object read failed
+    if(bezierPatch.vertices.size() == 0){
+        std::cout << "Read failed for control points " << argv[1] << std::endl;
+        return 0;
+    } // object read failed
 
-    // // create some default render parameters
-    // RenderParameters renderParameters(&bezierPatch);
+    // create some default render parameters
+    RenderParameters renderParameters(&bezierPatch);
 
-    // // use the object & parameters to create a window
-    // RenderWindow renderWindow(&bezierPatch, &renderParameters, argv[1]);
+    // use the object & parameters to create a window
+    RenderWindow renderWindow(&bezierPatch, &renderParameters, argv[1]);
 
-    // // create a controller for the window
-    // RenderController renderController(&bezierPatch, &renderParameters, &renderWindow);
+    // create a controller for the window
+    RenderController renderController(&bezierPatch, &renderParameters, &renderWindow);
 
-    // //  set the initial size
-    // renderWindow.resize(1600, 720);
+    //  set the initial size
+    renderWindow.resize(1600, 720);
 
-    // // show the window
-    // renderWindow.show();
+    // show the window
+    renderWindow.show();
 
-    // // set QT running
-    // return renderApp.exec();
+    // set QT running
+    return renderApp.exec();
     } // main()
